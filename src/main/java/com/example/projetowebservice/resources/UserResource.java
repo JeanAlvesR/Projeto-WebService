91588cafd906cfe0121f5aef7ace104bacc9ec84
Jean Alves Rocha <96813984+JeanAlvesR@users.noreply.github.com>
2022-03-05 14:45:33 -0300

    User entity and resource

---
+++ src/main/java/com/example/projetowebservice/resources/UserResource.java
@@ -0,0 +1,18 @@
+package com.example.projetowebservice.resources;
+
+import com.example.projetowebservice.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController      //Serve para falar que esta classe é um recurso Web que é implementado pelo controlador rest
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll() {
+        User u = new User(1L, "Maria", "Maria@gmail.com","99756328", "Senha");
+        return ResponseEntity.ok().body(u);
+    }
+}
